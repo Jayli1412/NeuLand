479f3a104fc0271c8ff6a5b3c5098fc587643bae
hdb1412 <jayli2558@gmail.com>
2022-05-31 17:02:33 +0900

    add test class

---
+++ Source/NeuLand/VSCodeTest.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VSCodeTest.h"
+
+// Sets default values
+AVSCodeTest::AVSCodeTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AVSCodeTest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVSCodeTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
